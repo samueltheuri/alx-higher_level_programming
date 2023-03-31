eb35e23bf7a0d963f1dfd732c77e9cedcb8ff048
samueltheuri <samueltheuri695@gmail.com>
2023-03-31 16:12:51 +0000

    task

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Script that takes in a URL, sens a request to URL, and display body"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1] as resp:
+            print(resp.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
